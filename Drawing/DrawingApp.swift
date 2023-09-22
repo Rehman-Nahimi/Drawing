e809cd4ef7474c8c9d921ab01c21e72503a04dce
Rehman Nahimi <rehman2000@live.com>
2023-09-22 17:48:50 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ray Nahimi on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
