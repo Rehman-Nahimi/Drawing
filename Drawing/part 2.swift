6421102071a1c83e6b532b1a5c636f244a4d6e94
Rehman Nahimi <rehman2000@live.com>
2023-09-23 23:55:08 +0100

    Starting part 2

---
+++ Drawing/part 2.swift
@@ -0,0 +1,20 @@
+//
+//  part 2.swift
+//  Drawing
+//
+//  Created by Ray Nahimi on 23/09/2023.
+//
+
+import SwiftUI
+
+struct part_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct part_2_Previews: PreviewProvider {
+    static var previews: some View {
+        part_2()
+    }
+}
