6a52b3ad3711fa9fec30691d4c66d9abeedfc5c5
Rehman Nahimi <rehman2000@live.com>
2023-09-24 19:20:08 +0100

    Added the remaining techniques and made a front page to easily access them

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,44 +7,33 @@
 
 import SwiftUI
 
-struct Flower : Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8){
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width/2))
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-}
+
 
 struct ContentView: View {
     
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    
+
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalWidth, in: -40...40)
-                .padding([.horizontal,.bottom])
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding([.horizontal])
-            
+        NavigationView {
+            List{
+                
+                NavigationLink(destination: Animate()) {
+                    Text("Shape Animator")
+                }
+                
+                NavigationLink(destination: Animatable_pair()) {
+                    
+                    Text("Animating pairs")
+                }
+                NavigationLink(destination: Front_Page()){
+                    Text("Flower Petal")
+                }
+                NavigationLink(destination: part_2()) {
+                    Text("Colour Slider")
+                }
+                
+            }
+            .navigationTitle("Different techniques:")
         }
     }
 }
